608729d203f078d485551c0b33cea8326c46bf44
Dominik Mikes <d.mikes@saa.at>
2022-07-30 12:19:10 +0200

    added wrapper logic for grid component

--- libs/todo/src/lib/components/todo-list/todo-list.component.ts
+++ libs/todo/src/lib/components/todo-list/todo-list.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { CellClickedEvent, ColDef, GridReadyEvent } from 'ag-grid-community';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss'],
 })
-export class TodoListComponent {
+export class TodoListComponent implements OnInit {
+  // Each Column Definition results in one Column.
+  public columnDefs: ColDef[] = [
+    { field: 'make' },
+    { field: 'model' },
+    { field: 'price' }
+  ];
 
+  // DefaultColDef sets props common to all Columns
+  public defaultColDef: ColDef = {
+    sortable: true,
+    filter: true,
+    editable: true
+  };
+
+  // Data that gets displayed in the grid
+  public rowData$!: Observable<any[]>;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.rowData$ = this.http
+      .get<any[]>('https://www.ag-grid.com/example-assets/row-data.json');
+  }
+  // Example load data from sever
+  // onGridReady(params: GridReadyEvent) {
+  //   this.rowData$ = this.http
+  //     .get<any[]>('https://www.ag-grid.com/example-assets/row-data.json');
+  // }
 }
--- libs/ui/grid/src/lib/components/grid/grid.component.ts
+++ libs/ui/grid/src/lib/components/grid/grid.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, ViewChild } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
 import { AgGridAngular } from 'ag-grid-angular';
 import { CellClickedEvent, ColDef, GridReadyEvent } from 'ag-grid-community';
 import { Observable } from 'rxjs';
@@ -10,40 +10,28 @@ import { Observable } from 'rxjs';
   styleUrls: ['./grid.component.scss'],
 })
 export class GridComponent {
- // Each Column Definition results in one Column.
- public columnDefs: ColDef[] = [
-  { field: 'make'},
-  { field: 'model'},
-  { field: 'price' }
-];
+  @Input() columnDefs!: ColDef[];
+  @Input() defaultColDef!: ColDef;
+  @Input() rowData$!: Observable<any[]>;
 
-// DefaultColDef sets props common to all Columns
-public defaultColDef: ColDef = {
-  sortable: true,
-  filter: true,
-};
+  // For accessing the Grid's API
+  @ViewChild(AgGridAngular) agGrid!: AgGridAngular;
 
-// Data that gets displayed in the grid
-public rowData$!: Observable<any[]>;
+  constructor(private http: HttpClient) { }
 
-// For accessing the Grid's API
-@ViewChild(AgGridAngular) agGrid!: AgGridAngular;
+  // Example load data from sever
+  onGridReady(params: GridReadyEvent) {
+    // this.rowData$ = this.http
+    //   .get<any[]>('https://www.ag-grid.com/example-assets/row-data.json');
+  }
 
-constructor(private http: HttpClient) {}
+  // Example of consuming Grid Event
+  onCellClicked(e: CellClickedEvent): void {
+    console.log('cellClicked', e);
+  }
 
-// Example load data from sever
-onGridReady(params: GridReadyEvent) {
-  this.rowData$ = this.http
-    .get<any[]>('https://www.ag-grid.com/example-assets/row-data.json');
-}
-
-// Example of consuming Grid Event
-onCellClicked( e: CellClickedEvent): void {
-  console.log('cellClicked', e);
-}
-
-// Example using Grid's API
-clearSelection(): void {
-  this.agGrid.api.deselectAll();
-}
+  // Example using Grid's API
+  clearSelection(): void {
+    this.agGrid.api.deselectAll();
+  }
 }
