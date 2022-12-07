f0d4a40f5a8456800a9de66a7ac27966d210bae4
Dominik Mikes <dominik.blau@gmail.com>
2022-12-07 21:33:53 +0100

    list and rmeove todos

--- libs/react-list/src/lib/react-list.tsx
+++ libs/react-list/src/lib/react-list.tsx
@@ -1,15 +1,38 @@
+import React from "react";
 import styles from './react-list.module.scss';
+import { ITodo } from "./todo";
+import { TODOS } from './todos.mock';
 
-/* eslint-disable-next-line */
-export interface ReactListProps { }
+// eslint-disable-next-line @typescript-eslint/ban-types
+class ReactList extends React.Component<any, any> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      todos: TODOS
+    }
+  }
+  removeTodo(id: number): any {
+    const filtered = this.state['todos'].filter((todo: ITodo) => { return todo.id !== id });
+    this.setState({
+      todos: filtered
+    });
+  }
+  getTodoRow(todo: ITodo) {
+    return <div key={todo.id}>{todo.description} - {todo.status}
+      <button data-todo-id={todo.id} onClick={() => this.removeTodo(todo.id)}>X</button>
+    </div>;
+  }
+  override render() {
+    return (
+      <div className={styles['container']}>
+        <div>TOOLBAR BUTTONS</div>
+        {this.state['todos'].map((todo: ITodo) => {
+          return this.getTodoRow(todo);
+        })}
+      </div>
+    );
+  }
 
-export function ReactList(props: ReactListProps) {
-  return (
-    <div className={styles['container']}>
-      <div>TOOLBAR BUTTONS</div>
-
-    </div>
-  );
 }
 
-export default ReactList;
+export { ReactList };
