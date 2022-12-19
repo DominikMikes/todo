de32d67933be8de49d38e6eb6b19a2c10db413bb
MrDeibl <dominik.blau@gmail.com>
2022-12-19 13:47:25 +0100

    crud react, changed id to string, added uuid

--- libs/todo/src/lib/mock-data/todo.mock.ts
+++ libs/todo/src/lib/mock-data/todo.mock.ts
@@ -2,25 +2,25 @@ import { ITodo } from "../model/todo";
 
 export const todos: ITodo[] = [
   {
-    id: 1,
+    id: '1',
     description: 'todo 1',
     status: 'In Progress',
     active: true
   },
   {
-    id: 2,
+    id: '2',
     description: 'todo 2',
     status: 'To Do',
     active: true
   },
   {
-    id: 3,
+    id: '3',
     description: 'todo 3',
     status: 'To Do',
     active: true
   },
   {
-    id: 4,
+    id: '4',
     description: 'todo 4',
     status: 'Cancled',
     active: false
